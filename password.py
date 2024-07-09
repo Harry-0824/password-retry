016e6c3fb46d99b93c905fcb8029106548f9249c
Harry <prince11290824@gmail.com>
2024-07-09 10:08:33 +0800

    basic ver done

---
+++ password.py
@@ -0,0 +1,15 @@
+pwd = 'a123456'
+i = 3
+while True:
+    password = input('請輸入使用者密碼: ')
+    if pwd == password:
+    	print('登入成功')
+    	break #逃出迴圈
+    else:
+    	i -= 1
+    	print('密碼錯誤! 還有',i,'次機會')
+    	if i == 0:
+    		print('拒絕存取')
+    		break
+    	
+
